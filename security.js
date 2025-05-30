0dd01bb5662cb22406cf71046dab02e75aa452bd
CodeX-gg <hasan_zali@hotmail.com>
2025-05-29 17:16:02 -0700

    Update security.js

--- security.js
+++ security.js
@@ -2,6 +2,6 @@ const password = 'geyTH6';
 if (localStorage.getItem('in') === null) {
   const input = prompt('Password:');
   if (input === password) {
-    localstorage.setItem('in', 'yes');
+    localStorage.setItem('in', 'yes');
   }
 }
